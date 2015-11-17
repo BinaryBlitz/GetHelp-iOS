ccb9ee656b94b8aeb46b3e8c40d010aad2a4a0d2
Dan Shevlyuk <danshevlyuk@icloud.com>
2015-11-17 17:43:36 +0300

    Initial commit.

---
+++ GetHelp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GetHelp
+//
+//  Created by Dan Shevlyuk on 17/11/15.
+//  Copyright © 2015 BinaryBlitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.4'
+use_frameworks!
+
+pod 'Alamofire'
+pod 'SwiftyJSON'
+pod 'MagicalRecord' 
+pod 'Fabric'
+pod 'Crashlytics'
