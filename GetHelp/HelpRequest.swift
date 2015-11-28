23f3413b24ec570a895cff93f0db454339a53dd1
Dan Shevlyuk <danshevlyuk@icloud.com>
2015-11-28 14:09:49 +0300

    Create presenters.

--- GetHelp/HelpRequest.swift
+++ GetHelp/HelpRequest.swift
@@ -18,8 +18,131 @@ class HelpRequest: Object {
   dynamic var faculty = ""
   dynamic var helpDescription = ""
   dynamic var deadline: NSDate?
+  dynamic var status = ""
+  
+  // oops
+  dynamic var email = ""
+  dynamic var duration = 0 // in minutes
+  dynamic var activityType = ""
+  dynamic var type = ""
+  var typeEnum: HelpType? {
+    return HelpType(rawValue: type)
+  }
+  
+  func presenter() -> HelpRequestPresenter? {
+    guard let type = typeEnum else {
+      return nil
+    }
+    
+    switch type {
+    case .Normal:
+      return NormalHelpRequestPresenter(helpRequest: self)
+    case .Express:
+      return ExpressHelpRequestPresenter(helpRequest: self)
+    }
+  }
 
 //  override class func primaryKey() -> String? {
 //    return "id"
 //  }
+}
+
+extension HelpRequest {
+  
+  //MARK: - NormalHelpRequestPresenter
+  
+  private class NormalHelpRequestPresenter: HelpRequestPresenter {
+    
+    private var helpRequest: HelpRequest
+    private let dateFormatter: NSDateFormatter
+    private let timeFormatter: NSDateFormatter
+    
+    init(helpRequest: HelpRequest) {
+      self.helpRequest = helpRequest
+      dateFormatter = NSDateFormatter()
+      dateFormatter.dateFormat = "dd.MM.yyyy"
+      timeFormatter = NSDateFormatter()
+      timeFormatter.dateFormat = "hh:mm"
+    }
+
+    var type: String {
+      return helpRequest.type
+    }
+    
+    var name: String {
+      return helpRequest.subject
+    }
+
+    var status: String {
+      return helpRequest.type
+      
+    }
+    var indicatorColor: UIColor {
+      return UIColor.orangeSecondaryColor()
+    }
+
+    var date: String {
+      guard let date = helpRequest.deadline else {
+        return ""
+      }
+      
+      return dateFormatter.stringFromDate(date)
+    }
+    
+    var time: String {
+      guard let date = helpRequest.deadline else {
+        return ""
+      }
+      
+      return timeFormatter.stringFromDate(date)
+    }
+  }
+  
+  //MARK: - ExpressHelpRequestPresenter
+  
+  private class ExpressHelpRequestPresenter: HelpRequestPresenter {
+    private var helpRequest: HelpRequest
+    private let dateFormatter: NSDateFormatter
+    private let timeFormatter: NSDateFormatter
+    
+    init(helpRequest: HelpRequest) {
+      self.helpRequest = helpRequest
+      dateFormatter = NSDateFormatter()
+      dateFormatter.dateFormat = "dd.MM.yyyy"
+      timeFormatter = NSDateFormatter()
+      timeFormatter.dateFormat = "hh:mm"
+    }
+
+    var type: String {
+      return helpRequest.type
+    }
+    
+    var name: String {
+      return helpRequest.subject
+    }
+
+    var status: String {
+      return helpRequest.type
+      
+    }
+    var indicatorColor: UIColor {
+      return UIColor.orangeSecondaryColor()
+    }
+
+    var date: String {
+      guard let date = helpRequest.deadline else {
+        return ""
+      }
+      
+      return dateFormatter.stringFromDate(date)
+    }
+    
+    var time: String {
+      guard let date = helpRequest.deadline else {
+        return ""
+      }
+      
+      return timeFormatter.stringFromDate(date)
+    }
+  }
 }
