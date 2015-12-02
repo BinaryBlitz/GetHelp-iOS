3ca648e83b6dcd840372b042a263c6516fe39730
Dan Shevlyuk <danshevlyuk@icloud.com>
2015-12-02 14:18:55 +0300

    Clean up.

--- GetHelp/CreateNewRequestTableViewController.swift
+++ GetHelp/CreateNewRequestTableViewController.swift
@@ -15,7 +15,7 @@ class CreateNewRequestTableViewController: UITableViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     typePresenters = HelpType.avaliableTypes().map { (type) -> HelpTypePresenter in
       return HelpTypePresenter(type: type)
     }
@@ -38,37 +38,37 @@ class CreateNewRequestTableViewController: UITableViewController {
 
     return cell
   }
-  
+
   //MARK: - UITableViewDelegate
-  
+
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     return typePresenters.count
   }
-  
+
   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     performSegueWithIdentifier("fillTheForm", sender: indexPath)
     tableView.deselectRowAtIndexPath(indexPath, animated: true)
   }
-  
+
   override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
     let presenter = typePresenters[section]
     return presenter.description
   }
 
   //MARK: - IBActions
-  
+
   @IBAction func cancelButtonAction(sender: AnyObject) {
     dismissViewControllerAnimated(true, completion: nil)
   }
-  
+
   //MARK: - Navigation
-  
+
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     guard let indexPath = sender as? NSIndexPath,
         destination = segue.destinationViewController as? RequestFormViewController else {
       return
     }
-    
+
     let presenter = typePresenters[indexPath.section]
     destination.type = presenter.type
   }
