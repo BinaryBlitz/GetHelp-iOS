a4676f545a33ebb5582cb5a8d9423bf47b19a7c4
Dan Shevlyuk <danshevlyuk@icloud.com>
2015-12-12 19:09:44 +0300

    Edit request form

--- GetHelp/RequestFormViewController.swift
+++ GetHelp/RequestFormViewController.swift
@@ -11,23 +11,23 @@ import Eureka
 import RealmSwift
 
 class RequestFormViewController: FormViewController {
-  
+
   var type: HelpType = HelpType.Normal
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     initialiseForm()
   }
-  
+
   //MARK: - Form
-  
+
   func initialiseForm() {
-    
+
     rowsSetup()
-    
+
     //MARK: - Section 1 (date and time)
-    
+
     switch type {
     case .Normal:
       form +++= DateTimeInlineRow("dateTimeRow") {
@@ -44,9 +44,9 @@ class RequestFormViewController: FormViewController {
         $0.value = NSDate().dateByAddingTimeInterval(60 * 60 * 3)
       }
     }
-    
+
     //MARK: - Section 2 (subject info)
-    
+
     switch type {
     case .Normal:
       form +++= TextFloatLabelRow("subjectRow") {
@@ -60,9 +60,9 @@ class RequestFormViewController: FormViewController {
         $0.title = "Тип работы"
       }
     }
-    
+
     //MARK: - Section 3 (university info)
-    
+
     form +++= Section()
     <<< PushRow<String>("schoolRow") {
       $0.title = "Вуз"
@@ -73,48 +73,116 @@ class RequestFormViewController: FormViewController {
     <<< TextFloatLabelRow("facilityRow") {
       $0.title = "Факультет"
     }
+    <<< PushRow<String>("courceRow") {
+      $0.title = "Курс"
+      $0.options = ["1", "2", "3", "4", "5", "6"]
+      $0.value = "1"
+      $0.selectorTitle = "Ваш курс"
+    }
 
-    //MARK: - Section 4 (Add content)
-    
+    //MARK: - Section 4 (description)
+
+    if type == .Normal {
+      form +++= Section("Email для отправки решения") {
+        $0.header
+      }
+      <<< TextFloatLabelRow("emailRow") {
+        $0.title = "email"
+      }
+    }
+
+    form +++= Section("Описание работы")
+    <<< TextAreaRow("descriptionRow") {
+      $0.placeholder = "Что это за работа?"
+    }
+
+    //MARK: - Section 5 (Add content)
+
     //TODO: Multiple photo selection. Alert with options. Create custome cell.
     form +++= ImageRow("photoRow") { row in
       row.title = "Приложить фотографии"
     }
-    
-    //MARK: - Section 5 (description)
-    
-    form +++= Section("Описание работы")
-    <<< TextAreaRow("desctiptionRow") {
-      $0.placeholder = "Что это за работа?"
-    }
   }
-  
+
   func rowsSetup() {
-    
+
     DateTimeInlineRow.defaultCellSetup = { cell, _ in
       cell.tintColor = UIColor.orangeSecondaryColor()
     }
-    
+
     TimeInlineRow.defaultCellSetup = { cell, _ in
       cell.tintColor = UIColor.orangeSecondaryColor()
     }
   }
-  
+
+  func presentAlertWithMessage(message: String) {
+    let alert = UIAlertController(title: nil, message: message, preferredStyle: .Alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+    presentViewController(alert, animated: true, completion: nil)
+  }
+
   @IBAction func submitButtonAction(sender: AnyObject) {
     guard let subject = form.rowByTag("subjectRow")?.baseValue as? String else {
+      print("subject!")
+      presentAlertWithMessage("Вы не указали предмет")
       return
     }
-    
+
+    guard let school = form.rowByTag("schoolRow")?.baseValue as? String else {
+      print("school!")
+      presentAlertWithMessage("Вы не выбрали где вы учитесь")
+      return
+    }
+
+    guard let facility = form.rowByTag("facilityRow")?.baseValue as? String else {
+      print("facility")
+      presentAlertWithMessage("Вы не указали ваш факультет")
+      return
+    }
+
+    guard let cource = form.rowByTag("courceRow")?.baseValue as? String else {
+      print("cource")
+      presentAlertWithMessage("Вы не выбрали ваш курс")
+      return
+    }
+
+    guard let helpDesctiption = form.rowByTag("descriptionRow")?.baseValue as? String else {
+      print("description!")
+      presentAlertWithMessage("Вы не добавили описание вашей работы")
+      return
+    }
+
+    guard let deadline = form.rowByTag("dateTimeRow")?.baseValue as? NSDate else {
+      print("deadline")
+      presentAlertWithMessage("Вы не выбрали дату работы")
+      return
+    }
+
     let helpRequest = HelpRequest()
-    
+
     helpRequest.type = type
     helpRequest.subject = subject
-    helpRequest.school = "ВШЭ"
-    helpRequest.faculty = "Программная инженерия"
+    helpRequest.school = school
+    helpRequest.faculty = facility
     helpRequest.email = "foo@bar.com"
-    helpRequest.deadline = NSDate()
-    helpRequest.activityType = "Контрольная"
-    
+    helpRequest.deadline = deadline
+    helpRequest.cource = cource
+    helpRequest.helpDescription = helpDesctiption
+
+    if let endTime = form.rowByTag("timeRow")?.baseValue as? NSDate {
+      helpRequest.duration = Int(endTime.timeIntervalSinceDate(deadline) / (60 * 60)) // in minutes
+    }
+
+    if let activityType = form.rowByTag("activityTypeRow")?.baseValue as? String
+        where type == .Express {
+
+      helpRequest.activityType = activityType
+    } else {
+      print("activity type")
+      presentAlertWithMessage("Вы не указали тип работы")
+      return
+    }
+
     let realm = try! Realm()
     do {
       try realm.write {
@@ -122,8 +190,9 @@ class RequestFormViewController: FormViewController {
       }
       presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     } catch {
-      //Something is really wrong. 
+      //Something is really wrong.
       //TODO: Alert about error
+      print("Something is really wrong.")
     }
   }
 }
